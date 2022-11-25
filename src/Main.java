3eb44418436020019d91633c62bd60d11b040ebe
alkaz07 <alkaz07@yandex.ru>
2022-11-25 23:51:32 +0300

    in

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        try {
+            Scanner scanner = new Scanner(new File("in.txt"));
+            int a = scanner.nextInt();
+            System.out.println(a);
+
+
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
