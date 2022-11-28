f21c72708812da906601bb2e3655d3ed91f37166
Kirill Morozov <kmmorozov@gmail.com>
2022-11-28 19:06:55 +0300

    тест

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-
+//новое изменение
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
